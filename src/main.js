f85a02b77ed19daa3513a2f707598ff8adf15e71
Andrew Bagger <andrewbagger@gmail.com>
2021-11-08 16:30:46 -0800

    Created stylesheet, Centered canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-let game = new Phaser.Game();
+let config = {
+    type: Phaser.CANVAS,
+    width: 320,
+    height: 240,
+    zoom: 2.5,
+    pixelArt: true
+}
+
+let game = new Phaser.Game(config);
