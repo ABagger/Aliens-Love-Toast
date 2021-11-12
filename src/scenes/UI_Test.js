8449b57b31baedd02e5b8eeae13f58f33d73efe6
Andrew Bagger <andrewbagger@gmail.com>
2021-11-11 17:00:06 -0800

    Updated UI, Added faux screen fold

--- src/scenes/UI_Test.js
+++ src/scenes/UI_Test.js
@@ -13,5 +13,13 @@ class UI_Test extends Phaser.Scene
     create()
     {
         let conceptSprite = this.add.sprite(0, 0, 'uiConcept').setOrigin(0,0);
+        let screenHide = this.add.rectangle(0,0,320,180, 0xffffff).setOrigin(0,0).setInteractive().setAlpha(0.001);
+
+        screenHide.on('pointerdown', () => {
+            if(screenHide.alpha < 1)
+                screenHide.setAlpha(1);
+            else
+                screenHide.setAlpha(0.001);
+        });
     }
 }
