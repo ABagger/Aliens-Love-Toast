f78f6df0351526b8b31e20bd1e543b33ccca0a72
Andrew Bagger <90874220+ABagger@users.noreply.github.com>
2022-10-24 19:24:05 -0700

    Testing dialogue prototype idea

---
+++ src/scenes/Dialog_Test.js
@@ -0,0 +1,8 @@
+let i = 0;
+
+const testDialog = {
+    name: "Test",
+    [`line${++i}`]:"This is the first line of dialog",
+    [`line${++i}`]:"This is the first line of dialog",
+    [`line${++i}`]:"And this is the 3rd line of dialog",
+};
--- src/scenes/UI_Test.js
+++ src/scenes/UI_Test.js
@@ -15,6 +15,8 @@ class UI_Test extends Phaser.Scene
         let conceptSprite = this.add.sprite(0, 0, 'uiConcept').setOrigin(0,0);
         let screenHide = this.add.rectangle(0,0,320,180, 0xffffff).setOrigin(0,0).setInteractive().setAlpha(0.001);
 
+        let testText = new Text(testDialog("line1"));
+
         screenHide.on('pointerdown', () => {
             if(screenHide.alpha < 1)
                 screenHide.setAlpha(1);
